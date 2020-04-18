61602d830ab7312df1db91a5f9c9387bde6383ed
Vishwas <Vishwas@DESKTOP-Q3DACNT>
2020-04-18 22:45:27 +0530

    Updated Repository

--- conference/src/main/java/com/pluralsight/model/Speaker.java
+++ conference/src/main/java/com/pluralsight/model/Speaker.java
@@ -1,6 +1,6 @@
 package com.pluralsight.model;
 
-public class Student {
+public class Speaker {
 
 	private String firstName;
 	private String lasttName;
--- conference/src/main/java/com/pluralsight/repository/HiebernateSpeakerRepositoryImpl.java
+++ conference/src/main/java/com/pluralsight/repository/HiebernateSpeakerRepositoryImpl.java
@@ -1,7 +1,27 @@
 package com.pluralsight.repository;
 
-public class HiebernateSpeakerRepositoryImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pluralsight.model.Speaker;
+
+public class HiebernateSpeakerRepositoryImpl implements SpeakerRepository {
 
 	
+	@Override
+	public List<Speaker> finaAll(){
+		
+		
+		List<Speaker> speakers = new ArrayList();
+	
+		Speaker speaker = new Speaker();
+		speaker.setFirstName("Mike");
+		speaker.setLasttName("Jagger");
+		
+		speakers.add(speaker);
+		
+		return speakers;
+	
+	}
 	
 }
