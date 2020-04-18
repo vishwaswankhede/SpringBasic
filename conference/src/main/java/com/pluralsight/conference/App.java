74c229ca30cccfc002eddeda7a5898bcaa239abd
vishwaslaxman.w <vishwaslaxman.w@NAGSEZ206805L.ad.infosys.com>
2020-04-18 20:24:48 +0530

    Adding Project

---
+++ conference/src/main/java/com/pluralsight/conference/App.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conference;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
