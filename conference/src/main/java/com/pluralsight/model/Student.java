fcda3cb5c1f541911029732e0e8b2705c99c4abf
vishwas <viablewish@gmail.com>
2020-04-18 21:09:13 +0530

    added student

---
+++ conference/src/main/java/com/pluralsight/model/Student.java
@@ -0,0 +1,24 @@
+package com.pluralsight.model;
+
+public class Student {
+
+	private String firstName;
+	private String lasttName;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLasttName() {
+		return lasttName;
+	}
+	public void setLasttName(String lasttName) {
+		this.lasttName = lasttName;
+	}
+	
+	
+	
+}
