945ed756cca986cf27874ff80adce516decd25b3
Vishwas <Vishwas@DESKTOP-Q3DACNT>
2020-04-19 00:05:55 +0530

    Setter based Injection

--- conference/src/main/java/com/pluralsight/conference/AppConfig.java
+++ conference/src/main/java/com/pluralsight/conference/AppConfig.java
@@ -1,12 +1,29 @@
 package com.pluralsight.conference;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
+import com.pluralsight.repository.SpeakerRepository;
+import com.pluralsight.service.SpeakerService;
+import com.pluralsight.service.SpeakerServiceImpl;
 
 @Configuration
 public class AppConfig {
 
+	@Bean(name = "speakerService")
+	public SpeakerService getSpeakerService() {
+		SpeakerServiceImpl speaker = new SpeakerServiceImpl();
+		speaker.setRepository(getSpeakerRepository());
+		return speaker;
+		
+	}
 	
-	
+	@Bean(name="speakerRepository")
+	public SpeakerRepository getSpeakerRepository() {
+		
+		return new HibernateSpeakerRepositoryImpl();
+	}
 	
 	
 }
